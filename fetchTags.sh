431118305167f8510975448ec927d6bc06c5bdab
Sören Mothes <soeren@staffbase.com>
2020-03-10 11:24:11 +0100

    remove sed command from git collect ticketIds command

--- fetchTags.sh
+++ fetchTags.sh
@@ -7,11 +7,11 @@ git fetch --tags
 currentTag=$(git describe --tags)
 
 tickets=$(git log --pretty="%s" \
-    $(git tag --sort=-creatordate | head -n 2 | tail -n 1)..$(git tag --sort=-creatordate | head -n 1 | tail -n 1) \
-    | grep -oE "[A-Z]+-[0-9]+" \
-    | sort \
-    | uniq \
-    | sed 's/^\|$/"/g'|paste -sd, -)
+  $(git tag --sort=-creatordate | head -n 2 | tail -n 1)..$(git tag --sort=-creatordate | head -n 1 | tail -n 1) |
+  grep -oE "[A-Z]+-[0-9]+" |
+  sort |
+  uniq |
+  paste -sd, -)
 
 echo "::set-output name=tIDs::$tickets"
 echo "::set-output name=tagName::$currentTag"
